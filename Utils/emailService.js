75ca2fc6608c5cc8fc7957149f00b12acd838dde
selvapriyan <selvapriyanbs@gmail.com>
2026-06-12 09:26:43 +0530

    Optimization of css

--- Utils/emailService.js
+++ Utils/emailService.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT),
     secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
